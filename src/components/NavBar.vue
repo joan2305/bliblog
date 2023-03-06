7768d73fd6de1708708fa93e4d60fcd8da4c343e
joan2305 <joanandreas875@gmail.com>
2023-03-06 17:06:32 +0800

    fix

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -6,16 +6,14 @@
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
           <ul class="navbar-nav me-auto mb-2 mb-lg-0">
             <li class="nav-item">
-              <router-link class="nav-link active" to="/posts"><a aria-current="page">Blogs</a></router-link>
+              <router-link class="nav-link active" to="/"><a aria-current="page">Blogs</a></router-link>
             </li>
-            <li class="nav-item">
-              <router-link class="nav-link active" to="/posts/post/3"><a aria-current="page">Add Blogs</a></router-link>
-            </li>
+            
           </ul>
-          <form class="d-flex" role="search">
+          <!-- <form class="d-flex" role="search">
             <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
             <button class="btn btn-outline-dark" type="submit">Search</button>
-          </form>
+          </form> -->
         </div>
       </div>
     </nav>
--- src/router/index.js
+++ src/router/index.js
@@ -8,7 +8,7 @@ Vue.use(Router)
 export default new Router({
   routes: [
     {
-      path: "/posts",
+      path: "/",
       name: "Posts",
       component: AppPosts,
     },
